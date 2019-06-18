08f5e9119c3775923b973e648ce322a9b3594b8d
John Connolly <jkconno@gmail.com>
2019-06-18 13:14:36 -0500

    steamlined code - object with regex patterns and simple for loop

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,80 +7,26 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
+
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
-
-  var prefixOne = Number(cardNumber.slice(0, 1));
-  var prefixTwo = Number(cardNumber.slice(0, 2));
-  var prefixThree = Number(cardNumber.slice(0, 3));
-  var prefixFour = Number(cardNumber.slice(0, 4));
-  var prefixSix = Number(cardNumber.slice(0, 6));
-  var length = cardNumber.length;
-
-//Diner's Club
-  if (length === 14 && (prefixTwo === 38 || prefixTwo === 39)){
-  	return "Diner's Club";
-
+  const cardMatch = {
+  "Diner's Club": new RegExp(/^3[8-9][0-9]{12}/),
+  'MasterCard': new RegExp(/^5[1-5][0-9]{14}/),
+  'China UnionPay': new RegExp(/^622[126-925][0-9]{10,13}|62[4-6][0-9]{13,16}|62[82-88][0-9]{12,15}/),
+  'American Express': new RegExp(/^34|37[0-9]{13}/),
+  'Switch': new RegExp(/^(4903|4905|4911|4936|6333|6759)[0-9]{12,15}|(564182|633110)[0-9]{10,13}/),
+  'Discover': new RegExp(/^6011[0-9]{12}|6011[0-9]{15}|64[4-9][0-9]{13}|64[4-9][0-9]{16}|65[0-9]{14}|65[0-9]{17}/),
+  'Maestro': new RegExp(/^(5018|5020|5038|6304)[0-9]{8,15}/),
+  'Visa': new RegExp(/^4[0-9]{12}|4[0-9]{15}|4[0-9]{18}/)
   };
 
-//American Express
-  if (length === 15 && (prefixTwo === 34 || prefixTwo === 37)){
-  	return 'American Express';
-  };
-
-//Switch
-//Switch always has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19. Heads up! Switch and Visa seem to have some overlapping card numbers - in any apparent conflict, you should choose the network with the longer prefix.
-
-	if ((prefixFour === 4903 || prefixFour === 4905 || prefixFour === 4911 || prefixFour === 4936) && (length >= 16 && length <= 19)){
-		return 'Switch';
-	};
-
-	if ((prefixSix === 564182 || prefixSix === 633110) && (length >= 16 && length <= 19)){
-		return 'Switch'
-	};
-
-	if ((prefixFour === 6333 || prefixFour === 6759) && (length >= 16 && length <= 19)){
-		return 'Switch';
-	};
-
-
-//Visa
-  if ((length === 13 || length === 16 || length ===  19) && prefixOne === 4){
-  	return 'Visa';
-  };
-
-//MasterCard
-  if ((prefixTwo >= 51 && prefixTwo <= 55) && length === 16){
-  	return 'MasterCard';
-  };
-
-//Discover
-  if (prefixFour === 6011 && (length === 16 || length === 19)){
-  	return 'Discover';
-  };
-
-  if ((prefixThree >= 644 && prefixThree <= 649) && (length === 16 || length === 19)){
-  	return 'Discover';
-  };
-
-  if (prefixTwo === 65 && (length === 16 || length === 19)){
-  	return 'Discover';
-  };
-
-//Maestro
-  if ((prefixFour === 5018 || prefixFour === 5020 || prefixFour === 5038 || prefixFour === 6304) && (length >= 12 && length <= 19)){
-  	return 'Maestro';
+  for (var key in cardMatch){
+    if (cardMatch[key].test(cardNumber)) {
+    // if (cardNumber.match(cardMatch[key])) {
+      return key;
+    }
   }
-
-//China UnionPay
-//"China UnionPay always has a prefix of 622126-622925, 624-626, or 6282-6288 and a length of 16-19."
-  if (((prefixSix >= 622126 && prefixSix <= 622925) || (prefixThree >= 624 && prefixThree <= 626) || (prefixFour >= 6282 && prefixFour <= 6288)) && (length >= 16 && length <= 19)) {
-  	return 'China UnionPay';
-  };
-
- };
-
+  return -1;
+  
+};
