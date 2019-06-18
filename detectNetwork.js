ef659f18bc3bb9e1e56adad377260c481734f961
John Connolly <jkconno@gmail.com>
2019-06-18 14:41:12 -0500

    corrected regex for China UnionPay

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,7 +13,7 @@ var detectNetwork = function(cardNumber) {
   const cardMatch = {
   "Diner's Club": new RegExp(/^3[8-9][0-9]{12}/),
   'MasterCard': new RegExp(/^5[1-5][0-9]{14}/),
-  'China UnionPay': new RegExp(/^622[126-925][0-9]{10,13}|62[4-6][0-9]{13,16}|62[82-88][0-9]{12,15}/),
+  'China UnionPay': new RegExp(/^622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5])[0-9]{10,13}|62[4-6][0-9]{13,16}|628[2-8][0-9]{12,15}/),
   'American Express': new RegExp(/^34|37[0-9]{13}/),
   'Switch': new RegExp(/^(4903|4905|4911|4936|6333|6759)[0-9]{12,15}|(564182|633110)[0-9]{10,13}/),
   'Discover': new RegExp(/^6011[0-9]{12}|6011[0-9]{15}|64[4-9][0-9]{13}|64[4-9][0-9]{16}|65[0-9]{14}|65[0-9]{17}/),
@@ -23,10 +23,9 @@ var detectNetwork = function(cardNumber) {
 
   for (var key in cardMatch){
     if (cardMatch[key].test(cardNumber)) {
-    // if (cardNumber.match(cardMatch[key])) {
       return key;
     }
   }
-  return -1;
+  return 'Unable to detect network';
   
 };
